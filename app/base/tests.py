37b3cc974e53d53e69771b68d5190d2bc91a05ea
Revelvol <111274957+Revelvol@users.noreply.github.com>
2023-01-17 21:17:58 +0700

    add test and configuration for docker compose

--- app/base/tests.py
+++ app/base/tests.py
@@ -1,16 +1,27 @@
-from django.test import TestCase , SimpleTestCase
+from django.test import SimpleTestCase
+from django.core.management import call_command
+from django.db.utils import OperationalError
 from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2Error
 
-from django.core.management import call_command
-from django.db.utils import OperationalError
 
-#test for django management command
-@patch('core.management.commands.wait_for_db.Command.check') #mocking the wait for db, biar di test ini ga actually edit the db
+@patch('base.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
+        """Test Waiting for Database if database ready"""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test Waiting for Database if Database return Operational Error"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True] #noqa
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+
+        patched_check.assert_called_with(databases=['default'])
