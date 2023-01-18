d3ecc1bf9b5ce4f710c3b84772e4d9310a2d701b
Revelvol <111274957+Revelvol@users.noreply.github.com>
2023-01-18 09:27:14 +0700

    save before adding venv manual

--- app/base/tests.py
+++ app/base/tests.py
@@ -1,4 +1,6 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from unittest.mock import patch
@@ -7,6 +9,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 
 @patch('base.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
+    """Test for Database """
     def test_wait_for_db_ready(self, patched_check):
         """Test Waiting for Database if database ready"""
         patched_check.return_value = True
@@ -25,3 +28,40 @@ class CommandTest(SimpleTestCase):
         self.assertEqual(patched_check.call_count, 6)
 
         patched_check.assert_called_with(databases=['default'])
+
+class ModelTest(TestCase):
+    """Test for Model"""
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpassword1'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email , expected)
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+class AdminTest(TestCase):
+    def test_admin(self):
+        pass
