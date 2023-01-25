64925815e8c24ac6bf8dad359070bf305d9e1cf9
Revelvol <111274957+Revelvol@users.noreply.github.com>
2023-01-25 21:08:11 +0700

    added production setting and local option, updated to django 4

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h)k2^2n(nu2(ra((p%^7@bsxe%y-9e^it)p)m_pr5$s2lx9h!$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
